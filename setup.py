8f620e23bd265418b0cf7a5885066f8b13d671c9
Samuel Cochran <sj26@sj26.com>
2015-07-03 16:16:05 +1000

    README funk

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='awscli-keyring',
-    version='0.0.1',
+    version='0.0.2',
     description='AWS command line credentials from OS X Keychain and other keyrings.',
-    long_description=open('README.md').read(),
+    long_description='AWS command line credentials from OS X Keychain and other keyrings.',
     author='Samuel Cochran',
+    author_email='sj26@sj26.com',
     url='https://github.com/sj26/awscli-keyring',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
@@ -17,12 +18,12 @@ setup(
         'keyring',
     ],
     license="MIT",
-    classifiers=(
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-    ),
+    ],
 )
