7fdfe7d4e4ff6c4b80bad64efc6461a9322a1569
Samuel Cochran <sj26@sj26.com>
2015-07-13 10:46:49 +1000

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awscli-keyring',
-    version='0.1.1',
+    version='0.1.2',
     description='AWS command line credentials from OS X Keychain and other keyrings.',
     long_description='AWS command line credentials from OS X Keychain and other keyrings.',
     author='Samuel Cochran',
